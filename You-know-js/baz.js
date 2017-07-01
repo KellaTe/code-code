76b09ec3b30d6a786558c6187ba371be127f56b1
KellaTe Ryan <kellete@163.com>
2017-07-01 16:53:27 +0800

    for README.md

---
+++ You-know-js/baz.js
@@ -0,0 +1,4 @@
+module foo from "foo";
+module bar from "bar";
+console.log(bar.hello("rhino"));
+foo.awesome();
---
+++ You-know-js/foo.js
@@ -0,0 +1,6 @@
+import hello from "bar";
+var hungry = "hippo";
+function awesome(){
+	console.log(hello(hungry).toUpperCase());
+}
+export awesome;
